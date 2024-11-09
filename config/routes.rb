8e3115678f34275bf77013c2e667ee00d69bd8ae
Denyyyys <denys.fokashchuk@gmail.com>
2024-11-09 18:10:39 +0100

    Add basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root "flowers#index"
+  root "pages#home"
+  devise_for :users
+  # root "flowers#index"
   resources :flowers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
